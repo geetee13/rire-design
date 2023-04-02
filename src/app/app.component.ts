a85d4b7dfd73a518aa14d038aa9ac4c33a3d31ce
geetee13 <tamas.guenther@gmail.com>
2023-04-02 15:03:45 +0200

    user slect as component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,5 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "Wer bist Du?"
-  gridColumns = 3
-  pilotList = [
-    {fullname: "Pflege/Empfang", nick: "Pflege/Empfang"},
-    {fullname: "Marco Amrein", nick: "Marco"},
-    {fullname: "Bettina Wyrsch", nick: "Bettina"},
-    {fullname: "Patrick Freudiger", nick: "Pädi"},
-    {fullname: "Szabolcs Szilagyi", nick: "Szabi"},
-  ]
 
-  toggleGridColumns() {
-    this.gridColumns = this.gridColumns === 3 ? 4 : 3
-  }
 }
